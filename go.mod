6650f94dad42d9e6d74a8f128e6e1edeb052b802
Shengwen YU <syu@ltu.edu>
2019-10-22 16:27:51 +0800

    [SOT-78] feat: add slack notification in chaos (#3)
    * feat: add slack notification in chaos
    
    * doc: add comment in terms of priority level

--- go.mod
+++ go.mod
@@ -5,8 +5,10 @@ go 1.12
 require (
 	github.com/btcsuite/btcd v0.0.0-20190629003639-c26ffa870fd8 // indirect
 	github.com/btcsuite/btcutil v0.0.0-20190425235716-9e5f4b9a998d
+	github.com/cenkalti/backoff v2.2.1+incompatible
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/ethereum/go-ethereum v1.9.1
 	github.com/kr/pretty v0.1.0 // indirect
-	golang.org/x/crypto v0.0.0-20190701094942-4def268fd1a4 // indirect
+	golang.org/x/crypto v0.0.0-20190701094942-4def268fd1a4
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 )
---
+++ network/slack/slack.go
@@ -0,0 +1,83 @@
+package slack
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"net/http"
+	"time"
+
+	"github.com/cenkalti/backoff"
+)
+
+type Field struct {
+	Title string `json:"title"`
+	Value string `json:"value"`
+	Short bool   `json:"short"`
+}
+
+type Attachment struct {
+	Color  string  `json:"color"`
+	Fields []Field `json:"fields"`
+}
+
+type SlackRequestBody struct {
+	Attachments []Attachment `json:"attachments"`
+}
+
+type Priority int
+
+// PRIORITY_0 is the most server / highest priority
+// PRIORITY_5 is the less server / lowest priority
+const (
+	PRIORITY_0 Priority = iota
+	PRIORITY_1
+	PRIORITY_2
+	PRIORITY_3
+	PRIORITY_4
+	PRIORITY_5
+)
+
+// @webhookUrl, the slack incoming webhookUrl to send the notification
+// @slackRequest, the slack message body
+// @retryTimeout, in seconds
+// @priority, the priority of the slack message
+// @priorityThreshold, the priority threshold determining whether to send this slack message based on the priority level
+func SendSlackNotification(webhookUrl string, slackRequest SlackRequestBody, retryTimeout int, priority Priority, priorityThreshold Priority) error {
+	if priority > priorityThreshold {
+		return nil
+	}
+
+	// marshal slack request and create slack webhook request
+	slackBody, _ := json.Marshal(slackRequest)
+	req, err := http.NewRequest(http.MethodPost, webhookUrl, bytes.NewBuffer(slackBody))
+	if err != nil {
+		return err
+	}
+	req.Header.Add("Content-Type", "application/json")
+	client := &http.Client{Timeout: 10 * time.Second}
+
+	// make slack webhook request with retry
+	b := backoff.NewExponentialBackOff()
+	b.MaxElapsedTime = time.Duration(retryTimeout) * time.Second
+	var resp *http.Response
+	sendNotification := func() error {
+		resp, err = client.Do(req)
+		return err
+	}
+	err = backoff.Retry(sendNotification, b)
+	if err != nil {
+		return err
+	}
+
+	// read response of slack webhook request
+	buf := new(bytes.Buffer)
+	_, err = buf.ReadFrom(resp.Body)
+	if err != nil {
+		return err
+	}
+	if buf.String() != "ok" {
+		return errors.New("non-ok response returned from Slack")
+	}
+	return nil
+}
